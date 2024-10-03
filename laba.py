336fd497500acd486b2ff5664b880aa3fab21d14
Maryan <maryanhazdyk@lfkhpp.ukr.education>
2024-10-03 21:44:17 +0300

    5zavd

--- laba.py
+++ laba.py
@@ -3,13 +3,36 @@ import io
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-bracket = input("Введіть символ дужки: ")
+week_number = int(input("Введіть номер тижня (1-2): "))
+day_number = int(input("Введіть день тижня (1-7): "))
 
-if bracket == '(' or bracket == ')':
-    print("Кругла дужка")
-elif bracket == '{' or bracket == '}':
-    print("Фігурна дужка")
-elif bracket == '[' or bracket == ']':
-    print("Квадратна дужка")
-else:
-    print("Це не дужка")
+if week_number == 1:
+    if day_number == 1:
+        print("Понеділок: Математика")
+    elif day_number == 2:
+        print("Вівторок: Фізика")
+    elif day_number == 3:
+        print("Середа: Хімія")
+    elif day_number == 4:
+        print("Четвер: Біологія")
+    elif day_number == 5:
+        print("П’ятниця: Інформатика")
+    elif day_number == 6:
+        print("Субота: Англійська")
+    else:
+        print("Неділя: Вихідний")
+elif week_number == 2:
+    if day_number == 1:
+        print("Понеділок: Географія")
+    elif day_number == 2:
+        print("Вівторок: Історія")
+    elif day_number == 3:
+        print("Середа: Математика")
+    elif day_number == 4:
+        print("Четвер: Фізика")
+    elif day_number == 5:
+        print("П’ятниця: Хімія")
+    elif day_number == 6:
+        print("Субота: Біологія")
+    else:
+        print("Неділя: Вихідний")
