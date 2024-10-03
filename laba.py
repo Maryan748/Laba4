6ecf246fd7272082ae98f3df85b5ca65ee8a7718
Maryan <maryanhazdyk@lfkhpp.ukr.education>
2024-10-03 21:38:20 +0300

    2zavd

--- laba.py
+++ laba.py
@@ -3,16 +3,15 @@ import io
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-initial_investment = 25000
-interest_rate_uah = 11.5 / 100
-interest_rate_usd = 4 / 100
-exchange_rate_buy = 27
-exchange_rate_sell = 28.6
+a = int(input("Введіть перше число: "))
+b = int(input("Введіть друге число: "))
+c = int(input("Введіть третє числоgh: "))
 
-amount_uah = initial_investment * (1 + interest_rate_uah)
-amount_usd = (initial_investment / exchange_rate_buy) * (1 + interest_rate_usd) * exchange_rate_sell
+unique_numbers = len(set([a, b, c]))
 
-if amount_uah > amount_usd:
-    print("Вигідніше вкласти в гривнях:", amount_uah)
+if unique_numbers == 1:
+    print(3)
+elif unique_numbers == 2:
+    print(2)
 else:
-    print("Вигідніше вкласти в доларах:", amount_usd)
+    print(0)
