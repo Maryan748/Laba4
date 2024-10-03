42e980d137b298337ab96a2ca2dbd6d439df0085
Maryan <maryanhazdyk@lfkhpp.ukr.education>
2024-10-03 21:42:47 +0300

    34avd

--- laba.py
+++ laba.py
@@ -3,15 +3,13 @@ import io
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-age = int(input("Введіть кількість прожитих років: "))
+bracket = input("Введіть символ дужки: ")
 
-if age < 6:
-    print("Ще не школяр")
-elif 6 <= age <= 9:
-    print("Початкова школа")
-elif 10 <= age <= 15:
-    print("Середня школа")
-elif 16 <= age <= 17:
-    print("Старша школа")
+if bracket == '(' or bracket == ')':
+    print("Кругла дужка")
+elif bracket == '{' or bracket == '}':
+    print("Фігурна дужка")
+elif bracket == '[' or bracket == ']':
+    print("Квадратна дужка")
 else:
-    print("Вже не школяр")
+    print("Це не дужка")
